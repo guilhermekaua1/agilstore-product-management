426c38eaf1f4162564c00a756ead4c1fdd7270c5
Guilherme Kaua <guilhermekaua10@gmail.com>
2025-01-07 10:54:04 -0300

    feat: configurar servidor inicial com Express

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("AgilStore Product Management System");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+//Middleware para interpretar JSON no body das requisições
+app.use(express.json());
+
+//Rota padrão
+app.get("/", (req, res) => {
+  res.send("Bem-vindo à API AgilStore!");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
